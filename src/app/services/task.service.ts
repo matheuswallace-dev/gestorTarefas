6a1da2b5ddd12075659b0c2f771e8d6d6dac4c81
Matheus Wallace <faconntheus1@gmail.com>
2025-03-31 16:50:15 -0300

    criando service e model

---
+++ src/app/services/task.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@angular/core';
+import { Task } from '../models/task';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  constructor() { }
+
+  private tasks: Array<Task> = [];
+
+  getTasks(): Array<Task> {
+
+    this.tasks = this.getFormLocalStorage();
+
+    return this.tasks;
+  }
+
+  getById(id: number): Task | undefined {
+
+    const task = this.tasks.find(c => c.id === id);
+
+    return task;
+  }
+
+  addTask(task: Task): void {
+
+    this.tasks.push(task);
+
+    this.saveToLocalStorage();
+  }
+
+  updateTask() {
+    this.saveToLocalStorage();
+  }
+
+  removeTask(task: Task) {
+    const index = this.tasks.indexOf(task);
+
+    if (index !== -1) {
+      //achou
+      this.tasks.splice(index, 1);
+    }
+  }
+
+  private saveToLocalStorage() {
+
+    const tasksJSON = JSON.stringify(this.tasks);
+
+    localStorage.setItem('tasks', tasksJSON);
+  }
+
+  private getFormLocalStorage(): Array<Task> {
+
+    const tasksJSON = localStorage.getItem('tasks');
+
+    if(!tasksJSON) {
+      //não achou
+      return new Array<Task>();
+    }
+
+    return JSON.parse(tasksJSON);
+  }
+}
